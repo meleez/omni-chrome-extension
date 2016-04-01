ffefc33ed6ada3e38707697d73f4e6256295e1b8
Andrew Carlson <acarl005@g.ucla.edu>
2016-04-01 15:14:44 -0700

    make it dynamic

--- examples/search-image/main.js
+++ examples/search-image/main.js
@@ -1,10 +1,7 @@
 module.exports = function searchImage(omni, query) {
-  console.log(query, omni);
   omni.removeItems();
   omni.addItems(
-    { title: 'corn', link: imgLink('corn'), subtitle: 'google images' },
-    { title: 'tigers', link: imgLink('tigers'), subtitle: 'google images' },
-    { title: 'kids', link: imgLink('kids'), subtitle: 'google images' }
+    { title: query, link: imgLink(query), subtitle: `Search Google images for ${query}` }
   );
   omni.sendFeedback();
 };
