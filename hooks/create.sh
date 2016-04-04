89514b9e3c95d797a1dd7fd1a94a785c18f1fb27
Andrew Carlson <acarl005@g.ucla.edu>
2016-04-04 16:20:16 -0700

    fix pre-commit hook

--- hooks/create.sh
+++ hooks/create.sh
@@ -1,4 +1,4 @@
 if [ ! -L .git/hooks/pre-commit ]; then
   echo Creating symlink to pre-commit hook.
-  ln -s hooks/pre-commit .git/hooks/pre-commit
+  ln -s ../../hooks/pre-commit .git/hooks/pre-commit
 fi
