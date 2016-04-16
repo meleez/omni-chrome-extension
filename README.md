9f7d3531f4b0f78a62941d64f24cda4b6b10b9a2
Alex Zai <azai91@gmail.com>
2016-04-16 15:09:45 -0700

    (feat) github package stores repos in local storage and queries local storage

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Omni-Chrome
 
+
+## How Do I Create Packages
+
+````
+
+function(omni, query) {
+        
+}
+
+
+````
+query is array with which is the first entire string minus the keyword
+
+## Omni Object
+
+
+
--- examples/github/main.js
+++ examples/github/main.js
@@ -1,43 +1,46 @@
-function authenticate(omni) {
-  // omni.addOauth({
-  //   oauth: "github",
-  //   client_secret: "a7d4c45e9dc563b07fb5e241f63c5e2f87241828",
-  //   client_id : "1649235ae4e380dd699c",
-  //   redirect_url: "http://localhost:1337?client_secret=a7d4c45e9dc563b07fb5e241f63c5e2f87241828&client_id=1649235ae4e380dd699c"
-  // });
-  console.log('authenticate');
-  var CALLBACK_URL = 'https://'+chrome.runtime.id+'.chromiumapp.org';
-  var AUTH_URL = 'https://github.com/login/oauth/authorize/?client_id=1649235ae4e380dd699c&redirect_uri='+CALLBACK_URL+'&scope=notifications'; 
-  
-  console.log('chrome', chrome);
-  chrome.identity.launchWebAuthFlow({
-    url: AUTH_URL,
-    interactive: true,
-  }, function(redirectURL) {
-    console.log(redirectURL);
-    var q = redirectURL.substr(redirectURL.indexOf('#')+1);
-    var parts = q.split('&');
-    for (var i = 0; i < parts.length; i++) {
-      var kv = parts[i].split('=');
-      if (kv[0] == 'access_token') {
-        token = kv[1];
-        console.log('token is', token);
-      }
-    }
-  }); 
-  
-  omni.sendFeedback();
+// function authenticate(omni) {
+//   // omni.addOauth({
+//   //   oauth: "github",
+//   //   client_secret: "a7d4c45e9dc563b07fb5e241f63c5e2f87241828",
+//   //   client_id : "1649235ae4e380dd699c",
+//   //   redirect_url: "http://localhost:1337?client_secret=a7d4c45e9dc563b07fb5e241f63c5e2f87241828&client_id=1649235ae4e380dd699c"
+//   // });
+//   omni.sendFeedback();
+// }
+
+
+function showOptions(omni) {
+
 }
 
+function updateCache(omni) {
+  chrome.storage.sync.get('github_access_token', (value) => {
+    fetch(`https://api.github.com/user/repos?access_token=${value.github_access_token}`, {
+      method: 'GET',
+      mode: 'cors-with-forced-preflight',
+    })
+    .then((response) => (response.json()))
+    .then((body) => {
+      const items = body.map((item) => ({ title: item.name, link: item.html_url }));
+      omni.saveCache('github_items', items, () => {
+        // omni.addItems(...items);
+        // omni.sendFeedback();
+      });
+    });
+  });
+}
 
+function showItemsInCache(omni, query) {
+  omni.getCache('github_items', (items) => {
+    omni.addItems(...items.filter((item) => item.title.toLowerCase().indexOf(query.toLowerCase()) >= 0));
+    omni.sendFeedback();
+  });
+}
 
-module.exports = function(omni, query) {
-
-  authenticate(omni);
-  // if (query.length > 0 && query[0] == '>') {
-
-  // }
-
-
+module.exports = function github(omni, query) {
+  console.log(query);
+  if (query && query[0] === '>') return showOptions(omni);
+  updateCache(omni);
+  showItemsInCache(omni, query);
 };
 
